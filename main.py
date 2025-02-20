e3a314769dcc260e7f0b0c6d6668baf204dd1436
Danel Rolon <danelrolon10@gmail.com>
2025-02-19 18:54:15 -0600

    Cracion de primeros endpoint y archivo

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import date
+
+app = FastAPI(
+    title="API de Gestión de Tareas",
+    description="API para gestionar una lista de tareas (To-Do List)",
+    version="1.0.1"
+)
+
+class Tarea(BaseModel):
+    id: int
+    titulo: str
+    descripcion: str
+    vencimiento: date
+    estado: bool 
+
+tareas = [
+    {
+        "id": 1,
+        "titulo": "Estudiar para el examen",
+        "descripcion": "Repasar los apuntes de TAI",
+        "vencimiento": "2024-02-14",
+        "estado": True
+    }
+]
+
+
+@app.get("/", tags=["Inicio"])
+def main():
+    return {'Hello World from FastAPI': 'API de Gestión de Tareas'}
+
+@app.get("/tareas", tags=["Tareas"])
+def obtener_todas_las_tareas():
+    return {"tareas": tareas}
